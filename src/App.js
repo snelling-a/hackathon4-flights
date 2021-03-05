63ec6039da70416b5842a5fd16515d640b95fa2e
snelling-a <snelling.andrew@gmail.com>
2021-03-05 09:12:06 +0100

    intialisation/react-router/sass/luxon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/App.scss';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>hackathon #4</h1>
+      <Router>
+        <Switch>
+          <Route />
+        </Switch>
+      </Router>
     </div>
   );
 }
